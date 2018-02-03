f430362b31ed0de11e87cb6fdd9922e3bd3e822b
Olena Ageyeva <eageeva@hotmail.com>
2018-02-03 16:00:11 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,9 +48,9 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
 
     if params[:artist_id]
-      author = Author.find_by(id: params[:author_id])
-      if author.nil?
-        redirect_to authors_path, alert: "Author not found."
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artist_path, alert: "Artist not found."
       else
         @post = author.posts.find_by(id: params[:id])
         redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
