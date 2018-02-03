a5da355a8f3464a05a55eb6954830269ece9e601
Olena Ageyeva <eageeva@hotmail.com>
2018-02-03 16:25:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists do
+  resources :artists, only: [:show, :edit, :index] do
     resources :songs, only: [:index, :show, :edit]
   end
   resources :songs
